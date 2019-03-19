4602f91e9eef1ae9e773063f833236ea38be5ee5
nikitavdovkin <43128087+nikitavdovkin@users.noreply.github.com>
2019-03-19 19:04:55 +0300

    Add files via upload

--- DBmanager.cs
+++ DBmanager.cs
@@ -6,14 +6,18 @@ using UnityEngine.SceneManagement;
 public class DBmanager : MonoBehaviour
 {
     public new InputField name;
-    public new InputField pass;
+    public InputField pass;
+    public new GameObject gameObject; //network ui
+    public Text text;
 
     public void Log()
     {
+        text.text = "";
         StartCoroutine(Login());
     }
     public void Reg()
     {
+        text.text = "";
         StartCoroutine(Register());
     }
 
@@ -23,16 +27,27 @@ public class DBmanager : MonoBehaviour
         form.AddField("name", name.text);
         WWW download = new WWW("http://nikitos25574.000webhostapp.com/login.php", form);
         yield return download;
+
         Debug.Log(download.text);
+        text.text += download.text + "\n";
+
         if (download.error != null)
         {
             Debug.Log("Error: " + download.error);
+            text.text += "Error: " + download.error + "\n";
             yield break;
         }
+
         Debug.Log("Server answer: " + download.text);
+        text.text += "Server answer: " + download.text + "\n";
+
         if (download.text != "User is not found.")
         {
-            SceneManager.LoadScene("firstmap");
+            if(gameObject.active == false)
+            {
+                gameObject.active = true;
+            }
+            SceneManager.LoadScene("MapPicker");
         }
         else
         {
@@ -42,17 +57,31 @@ public class DBmanager : MonoBehaviour
 	
     public IEnumerator Register()
     {
-        WWWForm form = new WWWForm();
-        form.AddField("name", name.text);
-        form.AddField("pass", pass.text);
-        WWW download = new WWW("http://nikitos25574.000webhostapp.com/register.php", form);
-        yield return download;
-        Debug.Log(download.text);
-        if (download.error != null)
+        if (name.text != null && pass.text != null && name.text.Length > 4 && pass.text.Length > 4)
         {
-            Debug.Log("Error: " + download.error);
-            yield break;
+            WWWForm form = new WWWForm();
+            form.AddField("name", name.text);
+            form.AddField("pass", pass.text);
+            WWW download = new WWW("http://nikitos25574.000webhostapp.com/register.php", form);
+            yield return download;
+
+            Debug.Log(download.text);
+            text.text += download.text + "\n";
+
+            if (download.error != null)
+            {
+                Debug.Log("Error: " + download.error);
+                yield break;
+            }
+
+            Debug.Log("Server answer: " + download.text);
+            text.text += "Server answer: " + download.text + "\n";
         }
-        Debug.Log("Server answer: " + download.text);
+        else
+        {
+            Debug.Log("Login or password == nul || .Lenght < 4");
+            text.text += "Login or password == nul || .Lenght < 4\n";
+        }
+        
     }
 }
